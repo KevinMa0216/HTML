c7ea841abbf01a00da1bf04a458c4127e99dc760
KevinMa0216 <159548454+KevinMa0216@users.noreply.github.com>
2024-02-12 14:25:41 -0500

    Add files via upload

---
+++ python/promedio.py
@@ -0,0 +1,10 @@
+nota1=3.2
+nota2=1.7
+nota3=4
+
+porcentaje1=nota1*0.20
+porcentaje2=nota2*0.30
+porcentaje3=nota3*0.50
+
+promedio=porcentaje1+porcentaje2+porcentaje3
+print ("el promedio del estudiante es", promedio)
---
+++ python/triangulo.py
@@ -0,0 +1,5 @@
+base=3
+altura=5
+area=(base*altura)/2
+print (area)
+
